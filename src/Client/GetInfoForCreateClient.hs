27b950a547f7814112b47bde21355b61e31aa6e6
LeviJunior21 <levi.pereira.junior@ccc.ufcg.edu.br>
2023-08-31 18:03:28 -0300

    Testes

--- src/Client/GetInfoForCreateClient.hs
+++ src/Client/GetInfoForCreateClient.hs
@@ -1,14 +1,17 @@
 module GetInfoForCreateClient where
+import System.IO (hFlush, stdout)
 
 getName :: IO String
 getName = do
   putStr "Digite o seu nome: "
+  hFlush stdout
   input <- getLine
   return input
 
 getAge :: IO Int
 getAge = do
   putStr "Digite a sua idade: "
+  hFlush stdout
   input <- getLine
   let age = (read input :: Int)
   return age
@@ -16,6 +19,7 @@ getAge = do
 getCPF :: IO Int
 getCPF = do
   putStr "Digite o seu CPF: "
+  hFlush stdout
   input <- getLine
   let cpf = (read input :: Int)
   return cpf
@@ -23,12 +27,14 @@ getCPF = do
 getEmail :: IO String
 getEmail = do
   putStr "Digite o seu e-mail: "
+  hFlush stdout
   input <- getLine
   return input
 
 getPassword :: IO Int
 getPassword = do
   putStr "Digite a sua senha: "
+  hFlush stdout
   input <- getLine
   let password = (read input :: Int)
   return password
--- src/Company/GetInfoForCreateCompany.hs
+++ src/Company/GetInfoForCreateCompany.hs
@@ -1,14 +1,17 @@
 module GetInfoForCreateCompany where
+import System.IO (hFlush, stdout) 
 
 getName :: IO String
 getName = do
   putStr "Digite o seu nome: "
+  hFlush stdout
   input <- getLine
   return input
 
 getAgeFounded :: IO Int
 getAgeFounded = do
   putStr "Digite o ano de fundação: "
+  hFlush stdout
   input <- getLine
   let age = (read input :: Int)
   return age
@@ -16,6 +19,7 @@ getAgeFounded = do
 getCNPJ :: IO Int
 getCNPJ = do
   putStr "Digite o seu CNPJ da empresa: "
+  hFlush stdout
   input <- getLine
   let cpf = (read input :: Int)
   return cpf
@@ -23,18 +27,21 @@ getCNPJ = do
 getActuation :: IO String
 getActuation = do
   putStr "Digite a área de atuação da empresa: "
+  hFlush stdout
   input <- getLine
   return input
 
 getDeclaration :: IO String
 getDeclaration = do
   putStr "Digite a declaração de missão: "
+  hFlush stdout
   input <- getLine
   return input
 
 getPassword :: IO Int
 getPassword = do
   putStr "Digite o código de verificação de 5 dígitos: "
+  hFlush stdout
   input <- getLine
   let password = (read input :: Int)
   return password
