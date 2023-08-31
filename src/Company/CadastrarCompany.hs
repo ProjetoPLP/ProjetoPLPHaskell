58396a5016937959e7b44b4f0274724aa707a568
LeviJunior21 <levi.pereira.junior@ccc.ufcg.edu.br>
2023-08-31 18:53:27 -0300

    Testes

--- src/Company/CadastrarCompany.hs
+++ src/Company/CadastrarCompany.hs
@@ -10,7 +10,7 @@ cadastrarCompany = do
     if length (show (password company)) == 5 then do
         if not (existCompanyByName (name company)) then do
             saveCompanyJSON "../Data/Companies.json" company
-            return ("Parabéns, a empresa " ++ name company ++ " foi cadastrada com sucesso!")
-        else return "Ocorreu um problema! A Empresa já está cadastrada!"
+            return ("\nParabéns, a empresa " ++ name company ++ " foi cadastrada com sucesso!")
+        else return "\nOcorreu um problema! A Empresa já está cadastrada!"
     else  
-        return "Ocorreu um problema! A senha deve ter 5 digitos."
+        return "\nOcorreu um problema! A senha deve ter 5 digitos."
