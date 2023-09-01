3d4b68465aced41309dd12e363eb393add46d913
LeviJunior21 <levi.pereira.junior@ccc.ufcg.edu.br>
2023-09-01 18:14:59 -0300

    Wallet

--- src/Client/GetAttrsClient.hs
+++ src/Client/GetAttrsClient.hs
@@ -4,3 +4,9 @@ import ModelClient
 
 getSaldo :: Int -> Float
 getSaldo id = (cash (getClientsByID id (getClientJSON "../Data/Clients.json")))
+
+getCPF :: Int -> Int
+getCPF id = (cpf (getClientsByID id (getClientJSON "../Data/Clients.json")))
+
+getNome :: Int -> String
+getNome id = (name (getClientsByID id (getClientJSON "../Data/Clients.json")))
