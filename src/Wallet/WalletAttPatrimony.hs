9fc8d3793dffbe956ce75463d4a00bcf59deafc5
Guilherme Alberto <100208535+GuilhermeAlz@users.noreply.github.com>
2023-09-12 12:50:40 -0300

    Wallet update functions

--- src/Wallet/WalletAttPatrimony.hs
+++ src/Wallet/WalletAttPatrimony.hs
@@ -1,9 +1,10 @@
 module Wallet.WalletAttPatrimony where
 
 import Client.ModelClient (Asset (companyID, qtd), Client (ident))
-import Client.GetSetAttrsClient (getAllAssets, setPatrimony, getPatrimony)
-
 import Company.GetSetAttrsCompany (getPrice)
+import Client.GetSetAttrsClient (getAllAssets, setPatrimony, getRow, getCol, getPatrimony)
+import Utils.MatrixUtils (writeMatrixValue)
+import Utils.GraphUtils (updateWalletGraphCandle, attClientLineRow)
 
 
 -- Atualiza o patrimônio de um cliente
@@ -30,8 +31,15 @@ attClientPatrimonyAux (x:xs) = do
 attAllClientsWalletPatrimonyGraph :: [Client] -> IO ()
 attAllClientsWalletPatrimonyGraph [] = return ()
 attAllClientsWalletPatrimonyGraph (x:xs) = do
+    let oldPatrimony = getPatrimony (ident x)
+    let newPatrimony = attClientPatrimonyAux (getAllAssets (ident x))
+    attClientWalletPatrimonyGraph (ident x) oldPatrimony newPatrimony
     attClientPatrimony (ident x)
-    --
-    -- atualizar gráfico
-    --
     attAllClientsWalletPatrimonyGraph xs
+
+
+attClientWalletPatrimonyGraph :: Int -> Float -> Float -> IO ()
+attClientWalletPatrimonyGraph idClient oldPatrimony newPatrimony = do
+    attClientLineRow idClient oldPatrimony newPatrimony
+    updateWalletGraphCandle ("./Client/Wallet/wallet" ++ show idClient ++ ".txt") (getRow idClient) (getCol idClient)
+    
