46c088d1126d923d041b39f5e67f0ef1e51fc1c7
Daniel Mantovani <daniel.mantovani047@gmail.com>
2023-09-12 13:53:42 -0300

    resolvido bug de poder cadastrar mais de 12 empreas, FINALMENTE

--- src/Company/CadastrarCompany.hs
+++ src/Company/CadastrarCompany.hs
@@ -4,32 +4,36 @@ import Company.SaveCompany
 import Company.ModelCompany
 import Company.CreateCompany
 
-cadastrarCompany :: IO Bool
-cadastrarCompany = do
-    company <- getNewCompany
-    if length (name company) <= 18 then
-        if length (actuation company) <= 29 then do
-            if length (declaration company) <= 86 then do
-                if length (cnpj company) == 14 then do
-                    if not (existCompanyByName (name company)) then do
-                        if length (code company) == 5 then do
-                            saveCompanyJSON "./Data/Companies.json" company
-                            return True
+cadastrarCompany :: Int ->  IO Bool
+cadastrarCompany limitCompanies = do
+    if limitCompanies < 12 then do
+        company <- getNewCompany
+        if length (name company) <= 18 then
+            if length (actuation company) <= 29 then do
+                if length (declaration company) <= 86 then do
+                    if length (cnpj company) == 14 then do
+                        if not (existCompanyByName (name company)) then do
+                            if length (code company) == 5 then do
+                                saveCompanyJSON "./Data/Companies.json" company
+                                return True
+                            else do
+                                putStrLn "Aviso: O código da empresa deve possuir 5 caracteres."
+                                return False
                         else do
-                            putStrLn "Aviso: O código da empresa deve possuir 5 caracteres."
+                            putStrLn "Aviso: A empresa já foi cadastrada."
                             return False
                     else do
-                        putStrLn "Aviso: A empresa já foi cadastrada."
+                        putStrLn "Aviso: O CNPJ não contém 14 digitos."
                         return False
                 else do
-                    putStrLn "Aviso: O CNPJ não contém 14 digitos."
+                    putStrLn "Aviso: A declaração de missão da empresa deve ter no máximo 86 caracteres."
                     return False
             else do
-                putStrLn "Aviso: A declaração de missão da empresa deve ter no máximo 86 caracteres."
+                putStrLn "Aviso: A área de atuação da empresa deve ter no máximo 29 caracteres."
                 return False
         else do
-            putStrLn "Aviso: A área de atuação da empresa deve ter no máximo 29 caracteres."
+            putStrLn "Aviso: O nome da empresa deve ter no máximo 18 caracteres."
             return False
     else do
-        putStrLn "Aviso: O nome da empresa deve ter no máximo 18 caracteres."
-        return False
+        putStrLn "Aviso: O limite máximo de empresas cadastradas foi atingido."
+        return False    
